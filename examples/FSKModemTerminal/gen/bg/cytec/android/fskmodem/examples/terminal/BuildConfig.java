4c7eede79e6fe2c749e3e5845beaa1dc8939cdfc
wami <wamiwami@live.jp>
2015-03-24 22:33:21 +0900

    test　fastvarsion

---
+++ examples/FSKModemTerminal/gen/bg/cytec/android/fskmodem/examples/terminal/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package bg.cytec.android.fskmodem.examples.terminal;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ library/android.fskmodem/gen/bg/cytec/android/fskmodem/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package bg.cytec.android.fskmodem;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
